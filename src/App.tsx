1b891f4f31ad102cb9ae6e81a865c9fef00dfde3
YashBhalodi <yashbhalodi.me@gmail.com>
2022-11-25 16:27:55 +0530

    split pane implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from "react";
 import Editor from "@monaco-editor/react";
 import _ from "lodash";
+import { SplitPane } from "react-multi-split-pane";
 
 const App: FC<{}> = () => {
   const [iframeContent, setIframeContent] = useState<string>("<div></div>");
@@ -22,7 +23,7 @@ const App: FC<{}> = () => {
 
   return (
     <div className="h-screen w-screen flex flex-1 flex-col">
-      <div className="grid grid-cols-2 h-screen">
+      <SplitPane split="vertical" minSize={50}>
         <iframe srcDoc={iframeContent} className={"w-full h-full"}></iframe>
 
         <Editor
@@ -38,7 +39,7 @@ const App: FC<{}> = () => {
           }}
           onChange={editorOnChange}
         />
-      </div>
+      </SplitPane>
     </div>
   );
 };
